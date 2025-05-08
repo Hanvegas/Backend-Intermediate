066cd155ef6ed0000b62ff9167a07682e7c8cd8a
Hanvegas <seanferdyan2@gmail.com>
2025-05-08 16:44:51 +0700

    Add auth middleware

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
 require('dotenv').config()
+const cookieParser = require('cookie-parser')
 const express = require('express')
 const app = express()
 
 app.use(express.urlencoded({ extended: true }))
+app.use(cookieParser())
 
 app.use('/movie', require('./routes/movie'))
 
